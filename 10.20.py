122f4aa85a17fb59cfe375238ff2f68a98bc5cf5
SunnyXiong321 <sxiong@madeira.org>
2025-10-21 09:06:32 -0400

    10.21
    my_schedule

--- 10.20.py
+++ 10.20.py
@@ -1,5 +1,14 @@
 my_schedule = open("schedule.txt")
 
-print(my_schedule)
+#print(my_schedule)
 
-print(my_schedule.read())
+#print(my_schedule.read())
+
+#print(my_schedule.read(6)) 
+
+'''
+print(my_schedule.readline()) #prints a full line of the file
+
+(my_schedule.readline()) #without printing, just moves the cursor/position of the file
+print(my_schedule.read(3))
+'''
